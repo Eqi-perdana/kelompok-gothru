6aabd5d41c893972908cb4517a1d4fa0f828366b
Eqi-perdana <perdanaeqi@gmail.com>
2025-05-09 09:37:11 +0700

    jsonwebtoken

---
+++ REST_API_AUTH_NODEJS/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    accessTokenSecret: 'MyAccessTokenSecret',
+     accessTokenExpiresIn: '30m',
+
+    refreshTokenSecret: 'myRefreshTokenSecret',
+   refreshTokenExpiresIn: '1w',
+
+   cacheTemporaryTokenPrefix: 'temp_token:',
+   cacheTemporaryTokenExpiresInSeconds: 180
+}
